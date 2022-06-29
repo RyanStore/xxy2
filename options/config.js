60aecb826a559bd20aff5785f88e325428f83b3e
ReffDessn <reffdesain@gmail.com>
2022-06-29 21:08:11 +0800

    xxy2

---
+++ options/config.js
@@ -0,0 +1,53 @@
+/**
+ - Created By Lexxy Official
+ - Base Ori Dika Ardnt
+*/
+
+const fs = require("fs")
+const chalk = require("chalk")
+
+// Other
+global.thum = fs.readFileSync("./plugins/media/image/thumb.jpg")
+global.log0 = fs.readFileSync("./plugins/media/image/thumb.jpg")
+global.err4r = fs.readFileSync("./plugins/media/image/thumb.jpg")
+global.locimg = fs.readFileSync("./plugins/media/image/loc.jpg")
+global.owner = ['6282252285143', '6282252285143']
+global.numberOwner = "6282252285143"
+global.group = "https://chat.whatsapp.com/DMqCCHW4Uvb2Kd4Cpus1vC"
+global.rkyt = []
+global.banUser = ['6281366679021@s.whatsapp.net']
+global.ntilink = ['120363043965529911@g.us','120363022988111568@g.us']
+global.wlcm = ['120363043965529911@g.us','120363023112413670@g.us','19054091487-1424650263@g.us','120363022988111568@g.us']
+global.gcrevoke = ['19054091487-1424650263@g.us']
+global.packname = "Yann-Md"
+global.author = "Ryan Store"
+global.sessionName = "session"
+global.footer = "© Ryan Store Id"
+global.linkgrupss = "https://chat.whatsapp.com/DMqCCHW4Uvb2Kd4Cpus1vC"
+global.antitags = true
+global.txtSewabot = "1-Minggu : Rp5.000\n2-Minggu : Rp.10.000\n1-Bulan : Rp.15.000\n2-Bulan : Rp.30.000\n3-Bulan : Rp40.000"
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.mess = {
+    success: 'Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    linkm: 'Linknya Mana ?',
+    ban: 'Kamu telah dibanned oleh owner, Jika ingin di unbanned chat owner'
+}
+global.limitawal = {
+    rakyat: "Infinity",
+    free: 100
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
